5c5ad566d025b4442a3f8981d2dfccdc01ca0700
rodilon <rodilon@brq.com>
2018-11-25 23:15:44 -0200

    Commit inicial

---
+++ app/src/main/java/br/com/androidpro/kfeedreader/DownloadImageTask.kt
@@ -0,0 +1,22 @@
+package br.com.androidpro.kfeedreader
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
+import android.widget.ImageView
+import java.net.URL
+import kotlin.coroutines.experimental.buildIterator
+
+class DownloadImageTask(val imageView: ImageView): AsyncTask<String, Void, Bitmap>() {
+    override fun doInBackground(vararg params: String?): Bitmap {
+        val url = params[0]
+        val stream = URL(url).openStream()
+        val bitmap = BitmapFactory.decodeStream(stream)
+
+        return bitmap
+    }
+
+    override fun onPostExecute(result: Bitmap?) {
+        imageView.setImageBitmap(result)
+    }
+}
